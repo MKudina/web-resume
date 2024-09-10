d1c90bbaee1cce99384b36a8476dadfea660ade4
KudinaPC <KudinaMD@Yandex.ru>
2024-09-10 17:57:55 +0300

    [WRP-15] Добавить WebPack в проект

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+window.React = React
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
